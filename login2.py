ee31c2c6c0da0c62b01ee2f3e1a343d45e6790a9
slashlinux <32353453+slashlinux@users.noreply.github.com>
2017-10-13 15:09:50 +0300

    Update login2.py

--- login2.py
+++ login2.py
@@ -1,41 +1,62 @@
 #!/bin/env python
 import pexpect
+import sys
+import time
+import csv
 
 username = raw_input("User: ")
-password = "passw"
 adduser = "set account name " + username
-t = 250
+
 
 
 f = open('ips')
 for line in f:
-    if t > 10:
+        string = line.replace("\n","")
 
-        #login
-        login = "ssh -o \"StrictHostKeyChecking no\" admin@"+line
-        child = pexpect.spawn (login)
-        child.expect ('.*password:')
-        child.sendline ('pass')
-        child.expect ('admin:')
+        if len(username) > 0:
+               #login
+                option_ssh = "-o \"ConnectTimeout=3\" -o \"StrictHostKeyChecking no\" "
+                p = pexpect.spawn('ssh ' + option_ssh + string.split()[1] + string.split()[2])
+                p.logfile = open("mylog", "a")
+                i = p.expect([pexpect.EOF, '[Pp]assword: '])
+                if i == 0:
+                        print 'No SSH connection for or invalid IP : %s(%s - %s)' % (string.split()[0],string.split()[2],string.split()[4])
+
+                if i == 1:
+
+                        p.sendline (string.split()[3])
+                        
+                        t = p.expect(['Permission denied, please try again.', 'admin:']) 
+                        if t == 0:
+                                print 'Wrong password for host: %s(%s - %s)' % (string.split()[0],string.split()[2],string.split()[4])
+                                #sys.exit (1)
+                        if t == 1:
+
+                                p.sendline(adduser)
+
+                                k = p.expect(['Account name already exists', 'Please enter the privilege level :'])
+
+                                if k == 0:
+                                        print 'User %s already exists: %s(%s - %s)' % (username,string.split()[0],string.split()[2],string.split()[4])
+
+                                if k == 1:
+                                        p.sendline('1')
+                                        p.expect ('Please enter the password :')
+
+                                        p.sendline(password)
+                                        p.expect ('re-enter to confirm :')
+
+                                        p.sendline(password)
+                                        p.expect ('admin:')
+
+                                        time.sleep(5)
+                                        p.sendline('exit')
+                       
+                                        print "Username %s was successfuly create:  %s(%s - %s)"   % (username,string.split()[0],string.split()[2],string.split()[4])
 
 
-        #adding user
-        child.sendline(adduser)
-        child.expect ('Please enter the privilege level :')
-        child.sendline('1')
-                
-        child.expect ('Please enter the password :')
-        child.sendline(password)    
-        child.expect ('re-enter to confirm :')
+        else:
 
-        child.sendline(password)
-        child.expect ('admin:')
+                print "Your input is empty...TRY AGAIN"
 
-        child.sendline('exit')
-
-        print "Username %s was successfuly create in %s"   % (username,line)
-
-
-    else:
-        print "bad luck"
 f.close()
